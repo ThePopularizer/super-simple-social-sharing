862ab7c54d5477dbb8640be70cfc28620f91d392
ThePopularizer <Tim@thepopularizer.com>
2020-01-18 11:23:04 +1300

    Images

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name:	ThePopularizer Social Sharing Buttons
+Plugin URI:		https://thepopularizer.com
+Description:	Social Buttons by ThePopularizer.
+Version:		1.0
+Author:			ThePopularizer
+Author URI:		https://thepopularizer.// COMBAK:
+License:		GPL-2.0+
+License URI:	http://www.gnu.org/licenses/gpl-2.0.txt
+
+This plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+This plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with This plugin. If not, see {URI to Plugin License}.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
